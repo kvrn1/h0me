47bcb855f77872085d6ad3ba3f0450101db18250
Louis Kvrn <100474049+kvrn1@users.noreply.github.com>
2025-02-24 14:25:06 +0100

    display buttons and add css filter (#2)

---
+++ src/components/Button88x31Comp.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps(['src', 'alt', 'href'])
+</script>
+
+<template>
+  <div class="w-[88px] h-[31px] bg-accent">
+    <a :href="href">
+      <img
+        :src="props.src"
+        :href="props.href"
+        :alt="props.alt"
+        class="grayscale opacity-80 hover:opacity-100 hover:filter-none"
+    /></a>
+  </div>
+</template>
