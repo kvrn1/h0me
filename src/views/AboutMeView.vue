520e7cc750e3672058df05a3ddad898b26220075
Louis Kvrn <louis.kvrn@gmail.com>
2025-02-24 17:43:25 +0100

    complete about me page

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <h1>About me</h1>
+    <h1>Me</h1>
+    <p>
+      Hi, please be welcome if you find your way on this website. The main goal of its existence is
+      to keep me busy and stop wasting my time on unwise distractions (hello doomscrolling of
+      death). I'm gonna post all kind of stuff related to projets I'm working on or things I want to
+      share. I cannot guarantee the high frequency of these publications though.
+    </p>
+    <p>
+      By the way my name is Louis, currently a CS student. For many years now, I've been flawlessly
+      using Linux systems and developing various projects for school or learning purpose (webapps,
+      android development, database exploitation, backend and REST API, frontend and UI/UX creation,
+      etc.). Django and VueJS are my stacks of choice for web development. I'm also confortable with
+      Java Spring or .NET framework (but I would prefer not to touch this crap or any of Microsoft's
+      products again in my life). I like my coffee like I like my software: without some big corpo's
+      AI shit in it (the pun is a bit strange but you get the idea).
+    </p>
+    <p>
+      Some of my interest these past years: cinema, classical literature, vinyls, nerdy tech-related
+      stuff, recently Formula 1 (can't escape the rabbit hole, please send help), a good walk in the
+      forest from time to time.
+    </p>
+
+    <h1>KVRN.zip</h1>
+    <p>
+      KVRN is just an abbreviation of my last name, nothing too mind breaking. The zip part sounds
+      cool and the subdomain is cheap so big win for me.
+    </p>
   </div>
 </template>
