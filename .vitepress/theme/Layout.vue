f08148632e340f02b5421fe9c3fadca53b84089a
Louis Kvrn <louis.kvrn@gmail.com>
2025-03-03 21:07:04 +0100

    refactor project and init vitepress

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Content, useData } from 'vitepress'
+import BlogPost from './pages/blog/BlogPost.vue'
+
+const { frontmatter } = useData()
+</script>
+
+<template>
+  <BlogPost v-if="frontmatter.layout ==='blog-post'" />
+  <Content v-else />
+</template>
--- docs/.vitepress/config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vitepress'
-
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: 'KVRN.zip',
-  description: 'Personal website',
-})
