575a009573e9b7324acf2d4deb9e63a852220222
Louis Kvrn <louis.kvrn@gmail.com>
2025-03-03 15:57:31 +0100

    init vitepress project

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useData } from 'vitepress'
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site, frontmatter } = useData()
+</script>
+
+<template>
+  <p>{{ site }}</p>
+  <p>{{ frontmatter }}</p>
+  <div v-if="frontmatter.home">
+    <h1>{{ site.title }}</h1>
+    <p>{{ site.description }}</p>
+    <ul>
+      <li><a href="/markdown-examples.html">Markdown Examples</a></li>
+      <li><a href="/api-examples.html">API Examples</a></li>
+    </ul>
+  </div>
+  <div v-else>
+    <a href="/">Home</a>
+    <Content />
+  </div>
+</template>
