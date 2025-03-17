5de1eae1a1f808b8826f439b3837e65321cc5a9c
Louis Kvrn <louis.kvrn@gmail.com>
2025-03-17 16:23:13 +0100

    add components from previous branch

---
+++ .vitepress/theme/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <nav class="flex space-x-2">
+    <span class="flex space-x-2">
+      <a href="#">...</a>
+      <p>/</p>
+      <a href="#">...</a>
+    </span>
+
+    <p>page name</p>
+  </nav>
+</template>
